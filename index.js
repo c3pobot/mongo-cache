0c259bd4d2a253ed26ff351ee6cdfdaef954b2eb
scuba75 <scuba75@gmail.com>
2026-05-15 20:00:15 -0500

    update

--- index.js
+++ index.js
@@ -2,34 +2,45 @@ const log = require('./logger')
 
 const { MongoClient } = require("mongodb");
 
-let connectionString = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}/?compressors=zlib&retryReads=true&retryWrites=true&maxPoolSize=200`
-if(process.env.MONGO_AUTH_DB) connectionString += `&authSource=${process.env.MONGO_AUTH_DB}`
-if(process.env.MONGO_REPSET) connectionString += `&replicaSet=${process.env.MONGO_REPSET}`
-
-let mongo = new MongoClient(connectionString), mongo_ready = false
-async function init(){
-  try{
-    await mongo.connect()
-    let status = await mongo.db('admin').command({ ping: 1 })
-    if(status.ok > 0){
-      mongo_ready = true
-      log.info(`mongo connection successful...`)
-      return
+module.exports = class MongoCache {
+  constructor({ connection_string, db_name, collections, cache_name }){
+    if(!db_name) throw `No db_name provided`
+    if(cacheName) log.setCacheName(cache_name || db_name)
+    this._mongo = new MongoClient(connection_string)
+    this._mongo_ready = false, this._collections = collections || [],  this._dbo = this._mongo.db(db_name)
+    this._init()
+  }
+  async _init(){
+    try{
+      await this._mongo.connect()
+      let status = await this._mongo.db('admin').command({ ping: 1 })
+      if(status.ok > 0){
+        log.info(`mongo-local connection successful...`)
+        return this._createTables()
+      }
+      setTimeout(this._init, 5000)
+    }catch(e){
+      log.error(e)
+      setTimeout(this._init, 5000)
     }
-    setTimeout(init, 5000)
-  }catch(e){
-    log.error(e)
-    setTimeout(init, 5000)
   }
-}
-init()
-module.exports.status = ()=>{
-  return mongo_ready
-}
-module.exports.MongoCache = class{
-  constructor(dbName){
-    this._dbo = mongo.db(dbName)
+  async _createTables(){
+    try{
+      if(!this._collections || this._collections?.length == 0) return this._mongo_ready = true
+      for(let i in this._collections){
+        if(!this._collections[i].expireSeconds) continue
+        await this._dbo.collection(this._collections[i].name)?.createIndex({ TTL: 1 }, { name: '_TTL', expireAfterSeconds: this._collections[i].expireSeconds } )
+        log.info(`Create TTL index for collection ${this._collections[i].name}`)
+      }
+      this._mongo_ready = true
+      return
+      setTimeout(this._createTables, 5000)
+    }catch(e){
+      log.error(e)
+      setTimeout(this._createTables, 5000)
+    }
   }
+
   async aggregate( collection, matchCondition, data = []){
     try{
       if(matchCondition) data.unshift({$match: matchCondition})
@@ -75,7 +86,15 @@ module.exports.MongoCache = class{
       log.error(e)
     }
   }
-
+  async dropIndex( collection, indexName){
+    try{
+      if(!collection || !indexName) return
+      let res = await this._dbo.collection( collection ).dropIndex(indexName)
+      if(res?.ok) return true
+    }catch(e){
+      log.error(e)
+    }
+  }
   async get(collection, matchCondition, project){
     try{
       let res = await this._dbo.collection( collection ).find( matchCondition, { projection: project } ).toArray()
@@ -103,12 +122,46 @@ module.exports.MongoCache = class{
 
   async set( collection, matchCondition, data ){
     try{
-      if(!data.TTL) data.TTL = new Date()
+      if(!data || !matchCondition || !collection) return
+      if(!data?.TTL) data.TTL = new Date()
       let res = await this._dbo.collection( collection ).updateOne( matchCondition, { $set: data }, { upsert: true } )
       delete data.TTL
-      return res
+      return res?.acknowledged
     }catch(e){
       log.error(e)
     }
+  }
+  async updateIndex( collection, keys, opts ){
+    try{
+      if(!collection || !keys || !opts?.name) return
+      let collections = await this._dbo.listCollections()?.toArray()
+      let indexCollection = collections.find(x=>x.name == collection)
+      if(!indexCollection?.name){
+        let created = await this._dbo.createCollection(collection)
+        if(created?.s?.namespace?.collection !== collection) return log.error(`error creating collection ${collection}...`)
+        log.debug(`collection ${collection} created...`)
+      }
+      let indexes = await this.listIndexes( collection )
+      let index = indexes?.find(x=>x.name == opts.name)
+      if(index?.key && JSON.stringify(index.key) == JSON.stringify(keys)){
+        if(!opts.expireAfterSeconds && !index.expireAfterSeconds) return true
+        if(opts.expireAfterSeconds && opts.expireAfterSeconds == index.expireAfterSeconds) return true
+        if(index.expireAfterSeconds && opts.expireAfterSeconds == index.expireAfterSeconds) return true
+      }
+      if(index?.name){
+        let dropped = await this.dropIndex(collection, opts.name)
+        if(!dropped) return log.error(`error dropping index index ${opts.name} for ${collection}...`)
+        log.debug(`dropped ${collection} index ${opts.name}...`)
+      }
+      let indexName = await this.createIndex( collection, keys, opts)
+      if(!indexName || indexName !== opts.name) return log.error(`error creating index ${opts.name} for ${collection}`)
+      log.debug(`created index ${opts.name} for ${collection}...`)
+      return true
+    }catch(e){
+      log.error(e)
+    }
+  }
+  status(){
+    return this._mongo_ready
   }
 }
--- logger.js
+++ logger.js
@@ -1,21 +1,33 @@
 let logLevel = process.env.LOG_LEVEL || 'info';
+let cache_name = 'mongo-cache'
 function getTimeStamp(timestamp){
   if(!timestamp) timestamp = Date.now()
   let dateTime = new Date(timestamp)
   return dateTime.toLocaleString('en-US', { timeZone: 'Etc/GMT+5', hour12: false })
 }
-export function error(err, table_name){
+function error(msg){
   try{
-    console.error(`${getTimeStamp(Date.now())} ERROR [mongo-cache] ${err}`)
-    if(err?.stack && logLevel == 'debug') console.error(err)
+    console.error(`${getTimeStamp(Date.now())} ERROR [${cache_name}] ${msg}`)
+    if(msg?.stack && logLevel == 'debug') console.error(msg)
   }catch(e){
     console.error(e)
   }
 }
-export function info(msg, table_name){
+function info(msg){
   try{
-    console.log(`${getTimeStamp(Date.now())} INFO [mongo-cache] ${msg}`)
+    console.log(`${getTimeStamp(Date.now())} INFO [${cache_name}] ${msg}`)
   }catch(e){
     console.error(e)
   }
 }
+function debug(msg){
+  try{
+    if(logLevel == 'debug') console.log(`${getTimeStamp(Date.now())} DEBUG [${cache_name}] ${msg}`)
+  }catch(e){
+    console.error(e)
+  }
+}
+function setCacheName(str){
+  cache_name = str || 'mongo-cache'
+}
+module.exports = { error, debug, info, setCacheName }
